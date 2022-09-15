6e2321d8af7265fb86384b53f70751f2b95a330f
Yortiz <yortizm1@miumg.edu.gt>
2022-09-14 19:48:49 -0600

    Add project files.

---
+++ Registros/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Title = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.login_button = New System.Windows.Forms.Button()
+        Me.password_label = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.user_label = New System.Windows.Forms.Label()
+        Me.User_txt = New System.Windows.Forms.TextBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Silver
+        Me.Panel1.Controls.Add(Me.Title)
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(1, 2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(906, 25)
+        Me.Panel1.TabIndex = 2
+        '
+        'Title
+        '
+        Me.Title.AutoSize = True
+        Me.Title.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Title.Location = New System.Drawing.Point(361, 0)
+        Me.Title.Name = "Title"
+        Me.Title.Size = New System.Drawing.Size(198, 21)
+        Me.Title.TabIndex = 4
+        Me.Title.Text = "APLICACION DE REGISTRO"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox2.BackgroundImage = Global.Registros.My.Resources.Resources.minimize_icon_23774
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox2.Location = New System.Drawing.Point(856, 0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(27, 25)
+        Me.PictureBox2.TabIndex = 3
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.BackgroundImage = Global.Registros.My.Resources.Resources.close_button_png_30241
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox1.Location = New System.Drawing.Point(883, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(23, 25)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Silver
+        Me.Panel2.Controls.Add(Me.login_button)
+        Me.Panel2.Controls.Add(Me.password_label)
+        Me.Panel2.Controls.Add(Me.TextBox1)
+        Me.Panel2.Controls.Add(Me.user_label)
+        Me.Panel2.Controls.Add(Me.User_txt)
+        Me.Panel2.Controls.Add(Me.PictureBox3)
+        Me.Panel2.Location = New System.Drawing.Point(246, 33)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(432, 524)
+        Me.Panel2.TabIndex = 3
+        '
+        'login_button
+        '
+        Me.login_button.BackColor = System.Drawing.Color.RoyalBlue
+        Me.login_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.login_button.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.login_button.ForeColor = System.Drawing.Color.White
+        Me.login_button.Location = New System.Drawing.Point(69, 420)
+        Me.login_button.Name = "login_button"
+        Me.login_button.Size = New System.Drawing.Size(283, 74)
+        Me.login_button.TabIndex = 5
+        Me.login_button.Text = "INGRESAR"
+        Me.login_button.UseVisualStyleBackColor = False
+        '
+        'password_label
+        '
+        Me.password_label.AutoSize = True
+        Me.password_label.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.password_label.ForeColor = System.Drawing.Color.DarkBlue
+        Me.password_label.Location = New System.Drawing.Point(71, 284)
+        Me.password_label.Name = "password_label"
+        Me.password_label.Size = New System.Drawing.Size(96, 21)
+        Me.password_label.TabIndex = 4
+        Me.password_label.Text = "Contraseña"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(69, 308)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(283, 54)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.UseSystemPasswordChar = True
+        '
+        'user_label
+        '
+        Me.user_label.AutoSize = True
+        Me.user_label.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.user_label.ForeColor = System.Drawing.Color.DarkBlue
+        Me.user_label.Location = New System.Drawing.Point(71, 172)
+        Me.user_label.Name = "user_label"
+        Me.user_label.Size = New System.Drawing.Size(69, 21)
+        Me.user_label.TabIndex = 2
+        Me.user_label.Text = "Usuario"
+        '
+        'User_txt
+        '
+        Me.User_txt.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.User_txt.Font = New System.Drawing.Font("Segoe UI", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.User_txt.ForeColor = System.Drawing.Color.White
+        Me.User_txt.Location = New System.Drawing.Point(69, 196)
+        Me.User_txt.Name = "User_txt"
+        Me.User_txt.Size = New System.Drawing.Size(283, 54)
+        Me.User_txt.TabIndex = 1
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox3.BackgroundImage = Global.Registros.My.Resources.Resources.perfil_del_usuario__1_
+        Me.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox3.Location = New System.Drawing.Point(156, 32)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(132, 123)
+        Me.PictureBox3.TabIndex = 0
+        Me.PictureBox3.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(907, 569)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Title As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents user_label As Label
+    Friend WithEvents User_txt As TextBox
+    Friend WithEvents password_label As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents login_button As Button
+End Class
---
+++ Registros/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Dim movement As Boolean = False
+    Private Sub Button1_Click(sender As Object, e As EventArgs) 
+        End
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) 
+        Me.WindowState = FormWindowState.Minimized
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        End
+    End Sub
+
+
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
+        movement = True
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel1.MouseMove
+        If (movement = True) Then Me.Location = Cursor.Position
+    End Sub
+
+    Private Sub Panel1_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel1.MouseUp
+        movement = False
+    End Sub
+
+End Class
