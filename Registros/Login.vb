1940e070ea58be65de0c3143aba8f14e380efe1c
Yortiz <yortizm1@miumg.edu.gt>
2022-09-21 18:34:13 -0600

    Se complementa con base de datos. root/root para inicio de sesión

--- Registros/Login.vb
+++ Registros/Login.vb
@@ -1,5 +1,9 @@
-﻿Public Class Login
+﻿Imports Microsoft.VisualBasic.ApplicationServices
+Imports MySqlConnector
+Imports MySqlConnector.MySqlConnection
+Public Class Login
     Dim movement As Boolean = False
+
     Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
         movement = True
     End Sub
@@ -20,13 +24,39 @@
     End Sub
 
     Private Sub login_button_Click(sender As Object, e As EventArgs) Handles login_button.Click
-        Me.Hide()
-        Form1.Show()
+        Dim conexion As MySqlConnection = New MySqlConnection
+        Dim comando As MySqlCommand = New MySqlCommand
+
+        comando.Connection = conexion
+
+        Try
+            conexion.ConnectionString = "Server=127.0.0.1;User ID=root;Password=root;Database=user_acces;Port=3308;"
+            conexion.Open()
+            'MsgBox("Conectado")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        comando.CommandText = "select * from usuario_acceso where usuario = '" + User_txt.Text + "' and contrasena = '" + TextBox1.Text + "'"
+
+        Dim r As MySqlDataReader
+
+        r = comando.ExecuteReader
+
+        If r.HasRows <> False Then
+            r.Read()
+            MsgBox("Bienvenido " + r.GetString("nombre_usuario"))
+            Me.Hide()
+            Form1.Show()
+        Else
+            MsgBox("Usuario y/o contrase;a Incorrectos")
+            User_txt.Text = ""
+            TextBox1.Text = ""
+
+        End If
+
+
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-        Me.Hide()
-        Form1.Show()
-    End Sub
 End Class
---
+++ Registros/Modules/Module1.vb
@@ -0,0 +1,37 @@
+﻿'IMPORTAMOS LAS LIBRERIAS NECESARIAS PARA LA CONEXIÓN A MYSQL/MARIADB
+Imports MySqlConnector.MySqlConnection
+'EMPIEZA EL MODULO
+Module Module1
+    ' CON PUBLIC SUB CREAMOS PROCEDIMIENTOS
+    Public Sub generaModelo()
+
+        ' METEMOS LA CADENA DE CONEXIÓN EN UNA VARIABLE
+        Dim cadenaConexion = "Server=127.0.0.1;User ID=root;Password=root;Database=person;Port=3308;"
+
+        ' CREAMOS LA CONEXIÓN CON LA CADENA DE CONEXIÓN
+        Dim connection = New MySqlConnector.MySqlConnection(cadenaConexion)
+
+        Try
+
+            ' SI SE ABRE LA CONEXIÓN MANDAMOS MENSAJE
+            connection.Open()
+            MsgBox("Conexion Correcta")
+            Dim comando = New MySqlConnector.MySqlCommand("select * from Person.person;", connection)
+            Dim resultado = comando.ExecuteReader
+            resultado.Read()
+            resultado.GetString("user")
+            resultado.GetString("password")
+            MessageBox.Show(resultado.GetInt64("dbid"))
+
+
+
+
+        Catch ex As Exception
+            ' SI NO SE ABRE LA CONEXIÓN MANDAMOS MENSAJE DE ERROR
+            MsgBox("ERROR " & ex.Message)
+
+            Exit Sub
+        End Try
+
+    End Sub
+End Module
